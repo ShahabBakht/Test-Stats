cd0c359dafb7f633856ef18f76671d8e76346626
ShahabBakht <shahab.bakhtiari@gmail.com>
2016-12-22 17:23:19 -0500

    added testMCMC

---
+++ TestMCMC.m
@@ -0,0 +1,20 @@
+%% Simulate data
+% y = theta * s + e;
+% e ~ N(0,1)
+% prior probablity of theta: theta ~ N(0,3)
+
+s = 10*ones(10,1);
+y = 2*s + randn(10,1);
+
+
+%% Metropolis Hasting
+
+theta0 = rand;
+p0 = normpdf(theta0,0,3);
+FF = theta0 * 10*ones(1000,1) + randn(1000,1);
+
+theta_propos = normrand(theta0,1);
+
+
+
+
