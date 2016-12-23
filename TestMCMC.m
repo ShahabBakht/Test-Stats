172fd03bbc349fd5cf436f484d370e095d91ef0e
root <root@localhost>
2016-12-23 09:42:27 -0500

    modified TestMCMC

--- TestMCMC.m
+++ TestMCMC.m
@@ -3,18 +3,47 @@
 % e ~ N(0,1)
 % prior probablity of theta: theta ~ N(0,3)
 
-s = 10*ones(10,1);
-y = 2*s + randn(10,1);
+s = 1*rand(10,1);
+y = (-2*s) + randn(10,1);
 
 
 %% Metropolis Hasting
+radii = 1;
 
 theta0 = rand;
 p0 = normpdf(theta0,0,3);
-FF = theta0 * 10*ones(1000,1) + randn(1000,1);
+FF = (theta0 * 1*rand(1000,1)) + randn(1000,1);
 LL0 = prod(arrayfun(@(t) sum((FF<(t + 2)) & (FF >= (t - 2)))/length(FF),y));
+p0_post = LL0 * p0;
 
-theta_propos = normrand(theta0,1);
+theta_hat(1) = theta0;
+p_post(1) = p0_post;
+
+for iter = 1:1e4
+theta_propos = normrnd(theta_hat(iter),.1);
+p = normpdf(theta_propos,0,3);
+FF = (theta_propos * 1*rand(1000,1)) + randn(1000,1);
+LL = prod(arrayfun(@(t) sum((FF<(t + radii)) & (FF >= (t - radii)))/length(FF),y));
+p_post(iter+1) = LL * p;
+
+if p_post(iter+1) > p_post(iter)
+    theta_hat(iter+1) = theta_propos;
+elseif p_post(iter+1) == 0
+    theta_hat(iter+1) = theta_hat(iter);
+else
+    if rand > p_post(iter+1)/p_post(iter)
+        theta_hat(iter+1) = theta_propos;
+    else
+        theta_hat(iter+1) = theta_hat(iter);
+    end
+end
+
+end
+    
+
+
+
+
 
 
 
